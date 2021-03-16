7cf8914816a1b288a2b51d5470372faa821bbe0a
barq <mohammad_barqawi@hotmail.com>
2021-03-16 17:12:06 +0200

    lab03

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # 201-Code-Project
+### the new features I just added on my project :
+1. 
+2. 
+3. 
+4. 
+5. 
+6. 
--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict';
-let i=0;
 
 
+/* This is a function :
 function correct(){
 // console.log('correct answer :D');
   alert('correct answer :D');
@@ -11,52 +11,107 @@ function inCorrect(){
   // console.log('Incorrect answer :(');
   alert('Incorrect answer :(');
 }
+*/
 
 
 
 
 
-
-let name1=prompt('what is your name?  ');
-alert('Welcome '+name1+' :D');
+let yourName=prompt('what is your name?  ');
+alert('Welcome '+yourName+' :D');
 alert('let\'s see if you know me :P');
+alert('you must enter yes or no ');
+
+let finalScore=0;
+
+
 let q1=prompt('Am I a person who loves soccer games ?');
-if(q1.toLowerCase() === 'yes'){
-  correct();
-
-}else{
-  inCorrect();
-}
-
+if (q1.toLowerCase()===('yes'||'y')){
+  alert('correct answer ! :D');
+  finalScore++;
+}else{alert('incorrect answer ! :(');}
 
 let q2=prompt('Am I a person who wears glasses?');
-if(q2.toLowerCase() === 'no'){
-  correct();
-}else{
-  console.log('incorrect answer :(');
-  inCorrect();
+if (q2.toLowerCase()===('no'||'n')){
+  alert('correct answer ! :D');
+  finalScore++;
+}else{alert('incorrect answer ! :(');}
+
+let q3=prompt('Am I a smoker person?');
+if (q3.toLowerCase()===('yes'||'y')){
+  alert('correct answer ! :D');
+  finalScore++;
+}else{alert('incorrect answer ! :(');}
+
+let q4=prompt('Am I social person ?');
+if (q4.toLowerCase()===('yes'||'y')){
+  alert('correct answer ! :D');
+  finalScore++;
+}else{alert('incorrect answer ! :(');}
+
+let q5=prompt('Am I nervous person ?');
+if (q5.toLowerCase()===('no'||'n')){
+  alert('correct answer ! :D');
+  finalScore++;
+}else{alert('incorrect answer ! :(');}
+
+
+
+
+//question 6
+
+
+let attempts=4;
+let correctAnswer = '27' ;
+while (attempts>0){
+  let age=prompt('how old am I ?');
+  if(age === correctAnswer){
+    alert('correct answer ! :D');
+    finalScore++;
+    break;
+  }else if(age>correctAnswer){
+    alert('too high');
+  } else {
+    alert('too low');
+  }
+  attempts--;
+  alert('you still have '+attempts+' left');
 }
+alert('the correct answer is 27 :)');
 
 
-let q3=prompt('Am I a smoker person? ');
-if(q3.toLowerCase() === 'yes'){
-  correct();
-}else{
-  inCorrect();
+
+
+// question 7
+let myFood=['shawarma','zinger','burger'];
+
+let attempts2=6;
+while (attempts2>0){
+  let food=prompt('you should guess what food do I prefer?');
+  if(food===myFood[0]){
+    alert('correct answer ! :D');
+    finalScore++;
+    break;
+  }else if(food===myFood[1]){
+    alert('correct answer ! :D');
+    finalScore++;
+    break;
+  } else if(food===myFood[2]){
+    alert('correct answer ! :D');
+    finalScore++;
+    break;
+  }
+  attempts2--;
+  alert('you still have '+attempts2+' left');
 }
-let q4=prompt('Am I social person ? ');
-if(q4.toLowerCase() === 'yes'){
-  correct();
-}else{
-  inCorrect();
-}
-
-let q5=prompt('Am I a nervous person? ');
-if(q5.toLowerCase() === 'yes'){
-  correct();
-}else{
-  inCorrect();
-}
+alert('the correct answer is '+myFood);
 
 
-alert('you have '+i+' correct answers :P ');
+
+
+alert('you final score is : '+finalScore+' :D ');
+
+
+
+
+
