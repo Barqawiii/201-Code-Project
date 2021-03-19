1b3202493ad3ca5330d2da4b5f1fca2e95eca244
barq <mohammad_barqawi@hotmail.com>
2021-03-19 22:45:05 +0200

    lab05b

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # 201-Code-Project
 ### the new features I just added on my project :
-1. 
-2. 
-3. 
-4. 
-5. 
+1. function called correct() 
+2. function called inCorrect() 
+3. function called theFinalScore()
+4. function called getUser()
+5. i use loops(while,for)
 6. 
--- js/app.js
+++ js/app.js
@@ -1,23 +1,16 @@
-
 'use strict';
-
 let finalScore = 0;
-
 function correct() {
-  // console.log('correct answer :D');
   alert('correct answer :D');
   finalScore++;
 }
 function inCorrect() {
-  // console.log('Incorrect answer :(');
   alert('Incorrect answer :(');
 }
 function theFinalScore() {
   alert('you final score is : ' + finalScore + ' :D ');
 }
 
-
-// create a function to get user's info
 function getUser() {
   let yourName = prompt('what is your name?  ');
   alert('Welcome ' + yourName + ' :D');
@@ -30,69 +23,39 @@ getUser();
 
 let q1 = prompt('Am I a person who loves soccer games ?');
 if (q1.toLowerCase() === ('yes' || 'y')) {
-  // i call the correct function here
   correct();
-  //alert('correct answer ! :D');
-  //finalScore++;
 } else {
-  // i call the inCorrect function here
   inCorrect();
-  //alert('incorrect answer ! :('); }
 
 }
 
 let q2 = prompt('Am I a person who wears glasses?');
 if (q2.toLowerCase() === ('no' || 'n')) {
-  // i call the correct function here
   correct();
-  //alert('correct answer ! :D');
-  //finalScore++;
 } else {
-  // i call the inCorrect function here
   inCorrect();
-  //alert('incorrect answer ! :('); }
 }
 
 let q3 = prompt('Am I a smoker person?');
 if (q3.toLowerCase() === ('yes' || 'y')) {
-  // i call the correct function here
   correct();
-  //alert('correct answer ! :D');
-  //finalScore++;
 } else {
-  // i call the inCorrect function here
   inCorrect();
-  //alert('incorrect answer ! :('); }
 }
 
 let q4 = prompt('Am I social person ?');
 if (q4.toLowerCase() === ('yes' || 'y')) {
-  // i call the correct function here
   correct();
-  //alert('correct answer ! :D');
-  //finalScore++;
 } else {
-  // i call the inCorrect function here
   inCorrect();
-  //alert('incorrect answer ! :('); }
 }
 
 let q5 = prompt('Am I nervous person ?');
 if (q5.toLowerCase() === ('no' || 'n')) {
-  // i call the correct function here
   correct();
-  //alert('correct answer ! :D');
-  //finalScore++;
 } else {
-  // i call the inCorrect function here
   inCorrect();
-  //alert('incorrect answer ! :('); }
 }
-
-
-
-
-//question 6
 
 function q6() {
   let attempts = 4;
@@ -115,8 +78,6 @@ function q6() {
 }
 q6();
 
-
-// question 7
 function q7() {
   let myFood = ['shawarma', 'zinger', 'burger'];
 
@@ -142,10 +103,6 @@ function q7() {
   alert('the correct answer is ' + myFood);
 }
 q7();
-
-
-
-//alert('you final score is : ' + finalScore + ' :D ');
 
 theFinalScore();
 
